b4560123bbe31321e8a12f929c182d976a743fe2
Xiaowei Chen <xiaoweichen@Xiaoweis-MacBook-Air.local>
2022-10-02 14:42:05 -0500

    Style the app.

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,13 +1,22 @@
 const CourseList = ({ courses }) => {
     let courseArr = Object.entries(courses);
     return (
-        courseArr.map((course, idx) => {
+        <div className="row">
+        {courseArr.map((course, idx) => {
             return (
-                <div key={idx}>
-                    <p>{ course[1].term } CS { course[1].number }: { course[1].title }</p>
+                <div className="col-sm-3">
+                <div className="card h-100" key={idx}>
+                    <div className="card-body">
+                    <h2 className="class-title">{ course[1].term } CS { course[1].number }</h2>
+                    <p className="card-text">{ course[1].title }</p>
+                    <p className="card-text">{ course[1].meets }</p>
+                    </div>
                 </div>
-            )
-        })
+                </div>
+                )
+            })
+        }
+        </div>
     )
 }
 export default CourseList;
