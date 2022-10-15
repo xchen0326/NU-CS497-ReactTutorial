4e9543612ec0829031ae4222154b676ec6d39674
Xiaowei Chen <xiaoweichen@Xiaoweis-MacBook-Air.local>
2022-10-14 17:54:39 -0700

    Validate the form.

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,32 +1,48 @@
+import { useState } from 'react';
 import {Button, Form} from 'react-bootstrap'
 
 const CourseForm = () => {
+    const [titleState, setTitleState] = useState('');
+    const [meetingTitle, setMeetingTitle] = useState('');
+    const [meetingTime, setMeetingTime] = useState('');
     var modal = document.getElementById("course-modal");
     const openFormModal = () => {
         modal.style.display = "block";
     }
     const submitForm = (e) => {
         e.preventDefault();
-        console.log("submit form");
+        if (meetingTitle.length < 2) {
+            setTitleState("The course title should have a length larger than 2.");
+        } else {
+            setTitleState("");
+        }
     }
     const closeForm = () => {
+        setMeetingTitle("");
+        setTitleState("");
+        document.getElementById("meetingTitle").value = '';
         modal.style.display = "none";
     }
 
     return (
         <div>
             <div id="course-modal" className="modal">
-                <div className="modal-content">
+                <div className="modal-content" style={{width: "50%"}}>
                     <span className="close" onClick={closeForm}>&times;</span>
                     <Form onSubmit={submitForm}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Course Title</Form.Label>
-                            <Form.Control type="input" placeholder="Enter course title" />
+                            <Form.Control id='meetingTitle' type="input" placeholder="Enter course title" onChange={
+                                (e) => setMeetingTitle(e.target.value)
+                            }/>
+                            <span style={{color: "red"}}>{titleState}</span>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Course Meeting Time</Form.Label>
-                            <Form.Control type="input" placeholder="Enter Course Meeting Time" />
+                            <Form.Control type="input" placeholder="Enter Course Meeting Time" onChange={
+                                (e) => setMeetingTime(e.target.value)
+                            } />
                         </Form.Group>
                         <Button variant="primary" onClick={closeForm}>
                             Cancel
